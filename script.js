ebf9fc7a4c257dcda3ed19ab9cac47dc339f35d4
Unknown <ayoub.assairh@imie.fr>
2018-01-12 11:42:18 +0100

    -Suppression de la version 1 du header, ajout de la seconde version

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
+<<<<<<< refs/remotes/origin/master
+=======
+  
+
+  // ScrollReveal.js
+>>>>>>> -Suppression de la version 1 du header, ajout de la seconde version
   ScrollReveal().reveal('.reveal',
   {
     reset: true,
@@ -7,4 +13,46 @@ $(document).ready(function(){
     duration: 3000,
   });
 
+<<<<<<< refs/remotes/origin/master
+=======
+  // Séléctionne toutes les id
+$('a[href*="#"]')
+  // Retire tous les liens qui ne pointe nulle part
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // L'evenement ne se déclenche que au clique
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Recherche l'élément à scroller vers
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Est ce qu'une cible pour scroller existe déjà ?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+
+
+>>>>>>> -Suppression de la version 1 du header, ajout de la seconde version
 });
